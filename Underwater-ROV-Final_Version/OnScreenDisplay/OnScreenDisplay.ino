bb6a2783b276c07fcff8aee826bf414d6148272b
divyap1 <divyap1@users.noreply.github.com>
2016-10-19 15:27:06 +1300

    Tested and working code with bugs fixed
    - fixed horizontal thrusters on when shouldn't be bug
    - fixed waiting for comms 
    - fixed battery low msg display

---
+++ Underwater-ROV-Final_Version/OnScreenDisplay/OnScreenDisplay.ino
@@ -0,0 +1,147 @@
+#include <TVout.h>
+#include <fontALL.h>
+#include <video_gen.h>
+#include <avr/pgmspace.h>
+
+// OSD chip cannot handle variables, so need to use defines
+#define W 136    // width of screen for osd
+#define H 96     // height of screen for osd
+
+#define HORIZON_START_X 5   // start x position for on screen display
+#define HORIZON_START_Y 80  // start y position for on screen display
+#define HORIZON_LENGTH 120
+#define TOP_X 2
+#define TOP_Y 8
+#define BATT_LENGTH 10
+#define BATT_HEIGHT 4
+#define LINE_LENGTH 16
+#define CENTER_X 60
+
+const char rov[] PROGMEM  = "ROV";
+const char pod[] PROGMEM  =  "POD";
+const char depth[] PROGMEM  = "DEPTH";
+const char temp[] PROGMEM  = "TEMP:";
+const char rovbattlow[] PROGMEM  ="ROV BATTERY LOW";
+const char podbattlow[] PROGMEM  ="POD BATTERY LOW";
+const char* const string_table[] PROGMEM = {rov, pod, temp,depth, rovbattlow, podbattlow}; //To move variables to SRAM 
+char buffer[15];
+
+TVout tv;   // On Screen Display screen variable
+
+// Data to show on display
+int temp_val = 0;
+int depth_val = 0;
+int angledeg = 0;
+int pod_volts = 0;
+int batt_volts = 0;
+
+void setup() {
+  Serial.begin(9600);
+  setupOSD();
+  delay(1000);
+} 
+
+void loop() {
+ Serial.end();
+ Serial.begin(9600);
+ delay(1000);
+  if(Serial.available() > 1) { 
+    angledeg = Serial.read();
+    pod_volts = Serial.read();
+    batt_volts = Serial.read();
+    temp_val = Serial.read();
+    depth_val = Serial.read();
+  }  
+  tv.fill(0); 
+  displayHorizon();
+  displayROVDepth();
+  displayROVTemp();
+  displayBatteryData();
+}
+
+void setupOSD() {
+  tv.begin(PAL, W, H);
+  tv.delay(500);
+  initOverlay();
+  tv.select_font(font4x6);
+}
+
+// Initialize ATMega registers for video overlay capability.
+// Must be called after tv.begin().
+void initOverlay() {
+  TCCR1A = 0;
+  // Enable timer1.  ICES0 is set to 0 for falling edge detection on input capture pin.
+  TCCR1B = _BV(CS10);
+  // Enable input capture interrupt
+  TIMSK1 |= _BV(ICIE1);
+  // Enable external interrupt INT0 on pin 2 with falling edge.
+  EIMSK = _BV(INT0);
+  EICRA = _BV(ISC01);
+}
+
+// Required to reset the scan line when the vertical sync occurs
+ISR(INT0_vect) {
+  display.scanLine = 0;
+}
+
+void displayHorizon(){
+  tv.draw_line(HORIZON_START_X, HORIZON_START_Y, HORIZON_LENGTH, HORIZON_START_Y, 1);
+  tv.draw_circle(60, HORIZON_START_Y, 15, WHITE, -1);
+  angledeg = angledeg - 90; // Value sent will be 90 degress greater than actual angle
+  float angle = (angledeg/180.0) *PI;
+  int x = cos(abs(angle)) * LINE_LENGTH;
+  int y = sin(abs(angle)) * LINE_LENGTH;
+  if (angledeg == 0){
+    tv.draw_line(CENTER_X - LINE_LENGTH, HORIZON_START_Y, CENTER_X + LINE_LENGTH, HORIZON_START_Y, 1);
+  } else if (angledeg < 0){   
+    tv.draw_line(CENTER_X + x , HORIZON_START_Y + y, CENTER_X - x, HORIZON_START_Y - y, 1);
+  } else if (angledeg > 0){   
+    tv.draw_line(CENTER_X - x, HORIZON_START_Y + y, CENTER_X + x, HORIZON_START_Y - y, 1);
+  }
+}
+
+// ROV Battery Update
+void displayBatteryData() {
+  strcpy_P(buffer, (char*)pgm_read_word(&(string_table[0]))); //Retrieving variables and copying to buffer
+  tv.print(TOP_X, TOP_Y, buffer);
+  drawBattery(22, TOP_Y, BATT_LENGTH, BATT_HEIGHT);
+  fillBattery(22, TOP_Y, batt_volts, BATT_HEIGHT); 
+  
+  // if ROV battery low, print message
+  if (batt_volts <= 2) {     
+    strcpy_P(buffer, (char*)pgm_read_word(&(string_table[4])));
+    tv.print(44, TOP_Y, buffer);
+  }
+  
+ // Pod Battery Update
+ strcpy_P(buffer, (char*)pgm_read_word(&(string_table[1])));
+  tv.print(TOP_X, TOP_Y + 8, buffer);
+  drawBattery(22, TOP_Y + 8, BATT_LENGTH, BATT_HEIGHT);
+  fillBattery(22, TOP_Y + 8, pod_volts, BATT_HEIGHT); 
+  
+  if (pod_volts <= 1) { 
+    strcpy_P(buffer, (char*)pgm_read_word(&(string_table[5])));
+    tv.print(44, TOP_Y + 8, buffer);
+  }
+}
+
+void displayROVTemp() {
+  strcpy_P(buffer, (char*)pgm_read_word(&(string_table[2])));
+  tv.print(TOP_X, TOP_Y + 16, buffer);
+  tv.print(24, TOP_Y + 16, temp_val); 
+}
+
+void displayROVDepth(){
+ strcpy_P(buffer, (char*)pgm_read_word(&(string_table[3])));
+ tv.print(TOP_X, TOP_Y + 24, buffer);
+ tv.print(28, TOP_Y + 24, depth_val);       
+}
+
+void drawBattery(int x0, int y0, int x1, int y1) {
+  tv.draw_rect(x0, y0, x1, y1, 1, 0);  // x, y, width, height, no fill
+}
+
+void fillBattery(int x0, int y0, int x1, int y1){
+  tv.draw_rect(x0, y0, x1, y1, 1, 1);  // x, y, width, height, fill
+}
+
---
+++ Underwater-ROV-Final_Version/ResearchPod/ResearchPod.ino
@@ -0,0 +1,253 @@
+#include <SPI.h>
+#include <SD.h>
+#include <Wire.h>
+#include <SoftEasyTransfer.h>
+#include <EasyTransfer.h>
+#include <BH1750.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <SoftwareSerial.h>    //we have to include the SoftwareSerial library, or else we can't use it
+
+#define ONE_WIRE_BUS_1 4
+
+//Hardware pin definitions
+#define UVOUT A0 //Output from the sensor (ML8511)
+#define REF_3V3 A1 //3.3V power on the Arduino board (ML8511)
+#define REF_5V A2
+#define TURBIDITY_OUT A7 
+#define chipSelect 10 //SPI Chip select pin
+
+//#################AS Conductivity##############################
+                       
+#define rx 2                                          //define what pin rx is going to be for conductivity
+#define tx 3                                          //define what pin tx is going to be  for conductivity
+SoftwareSerial ASSerial(rx, tx);            //define how the soft serial port is going to communicate with condiuctivity sensor Micro
+String inputstring = "";                              //a string to hold incoming data from the PC
+String sensorstring = "";                             //a string to hold the data from the Atlas Scientific product
+boolean input_string_complete = false;                //have we received all the data from the PC
+boolean sensor_string_complete = false;               //have we received all the data from the Atlas Scientific product
+
+//#################AS Conductivity################################
+
+BH1750 lightMeter; //Create Light meter object
+
+bool sdCardAvailable = false;
+String fileName;
+OneWire oneWire_in(ONE_WIRE_BUS_1);
+DallasTemperature tempSensor(&oneWire_in);
+
+//##################Communcation############################
+//SoftwareSerial podSerial(7,8);
+//SoftEasyTransfer ETin, ETout;
+EasyTransfer ETin, ETout;
+
+//Turbidity Calibration
+float turbidity_offset= 0.00;
+
+struct RESEARCH_POD_RECEIVE_DATA {
+  int ROVPressure;  // ROV depth reading
+} podDataIn;
+
+struct RESEARCH_POD_SEND_DATA {
+  int PodVolts;
+} podDataOut;
+
+float current;
+float voltage;
+//##########################################################
+
+void setup() {                                        //set up the hardware
+  /*Atlas Scientific Conductivity*/
+  Serial.begin(9600);                                 //set baud rate for the hardware serial port_0 to 9600
+  ASSerial.begin(9600);                               //set baud rate for the software serial port to 9600
+  inputstring.reserve(10);                            //set aside some bytes for receiving data from the PC
+  sensorstring.reserve(30);                           //set aside some bytes for receiving data from Atlas Scientific product
+
+  /*ROV Communication*/
+  ETin.begin(details(podDataIn), &Serial);  
+  ETout.begin(details(podDataOut), &Serial);
+
+  /*SD Card Initializing Code*/
+  if (!SD.begin(chipSelect)) {
+    sdCardAvailable = false;
+  } else {
+    sdCardAvailable = true;
+    int dataNumber = 0;
+    fileName = "data" + String(dataNumber) + ".csv";
+    while(SD.exists(fileName)){
+      dataNumber ++;
+      fileName = "data" + String(dataNumber) + ".csv";
+      if(dataNumber >99){
+        dataNumber = 0;
+        fileName = "data" + String(dataNumber) + ".csv";
+        break;
+      }
+    }
+    
+    File SD_file = SD.open(fileName, FILE_WRITE);
+        // if the file is available, write to it:
+        if (SD_file) {
+          SD_file.println("Time,Temp,Light,Turbidity,UV,Current,Voltage,EC,TSD,SAL,GRAV");
+          SD_file.close();;
+        }
+  }
+  
+  /*DS18b20 Dallas one wire*/
+  tempSensor.begin();
+
+  /*BH1750 Light Meter*/
+  lightMeter.begin();
+
+  /*UV ML8511*/
+  pinMode(UVOUT, INPUT);
+  pinMode(REF_3V3, INPUT);
+  
+  /*TSD10 Calibration*/
+  delay(10);
+  float turbidity;
+  float ntu;
+  turbidity = (float)averageAnalogRead(TURBIDITY_OUT); 
+  turbidity *= 0.00488; // 5/1024 = 0.00488 --> converts to voltage  
+  turbidity_offset = turbidity - 4.3856;  
+}
+
+void loop() {
+  String datastring = "";
+
+  /*ROV Communications*/
+  ETout.sendData();
+  
+  if(Serial.available() >1){    
+    String command;
+    command = Serial.readString();
+    ASSerial.print(command); 
+  }
+
+  if (input_string_complete) {                        //if a string from the PC has been received in its entirety
+    ASSerial.print(inputstring);                      //send that string to the Atlas Scientific product
+    ASSerial.print('\r');                             //add a <CR> to the end of the string
+    inputstring = "";                                 //clear the string
+    input_string_complete = false;                    //reset the flag used to tell if we have received a completed string from the PC
+  }
+
+  if (ASSerial.available() > 0) {                     //if we see that the Atlas Scientific product has sent a character
+    char inchar = (char)ASSerial.read();              //get the char we just received
+    sensorstring += inchar;                           //add the char to the var called sensorstring
+    if (inchar == '\r') {                             //if the incoming character is a <CR>
+      sensor_string_complete = true;                  //set the flag
+    }
+  }
+
+
+  if (sensor_string_complete == true) {               //if a string from the Atlas Scientific product has been received in its entirety
+    if (isdigit(sensorstring[0]) == false) {          //if the first character in the string is a digit
+      // do nothing
+    }
+    else {                                            //if the first character in the string is NOT a digit
+      datastring += millis()/1000.00;                       //Add timestamp to string
+      datastring += ',';
+      datastring += get_temp();                       //Add temp to string
+      datastring += ',';
+      datastring += lightMeter.readLightLevel();      //Add light to string
+      datastring += ',';
+      datastring += get_turbidity();                  //Add turbidity to string
+      datastring += ',';
+      datastring += get_UV();                         //Add UV to string
+      datastring += ',';
+      datastring += get_current();
+      datastring += ',';
+      datastring += get_voltage();
+      datastring += ',';
+      datastring += sensorstring;                     //Add AS conductivity to string
+      if(sdCardAvailable){
+        File SD_file = SD.open(fileName, FILE_WRITE);
+        // if the file is available, write to it:
+        if (SD_file) {
+          SD_file.print(datastring);
+          SD_file.close();
+        }
+      }
+    }
+    sensorstring = "";                                //clear the string
+    sensor_string_complete = false;                   //reset the flag used to tell if we have received a completed string from the Atlas Scientific product
+  }
+  podDataOut.PodVolts = get_scaled_pod_volts();
+}
+
+int get_scaled_pod_volts(){
+  float volts = get_voltage();
+  float conversion = -1 * (log(volts) - 2.1152)/0.2341;
+  int value = 10 * (1 - conversion); 
+  int lowerBound = max(value, 0);
+  int actual = min(lowerBound, 10);
+  return actual; 
+}
+
+//returns temperature value
+float get_temp(){
+  tempSensor.requestTemperatures();
+  return tempSensor.getTempCByIndex(0);
+}
+
+//returns turbidity value
+float get_turbidity(){
+  float turbidity;
+  float ntu;
+  turbidity = (float)averageAnalogRead(TURBIDITY_OUT);
+  turbidity *= 0.00488; // 5/1024 = 0.00488 --> converts to voltage
+  turbidity -= turbidity_offset;
+  
+  /*Linear Approximation of Turbidity*/
+  if (turbidity > 2.8)
+    ntu = (turbidity-4.3856)/-0.0011;
+  else if(turbidity >1.8)
+    ntu = (turbidity - 3.8183)/-0.0007;
+  else
+    ntu = (turbidity - 3.1929)/-0.0005;
+  return max(0.00,ntu);
+}
+
+//returns UV value
+float get_UV(){
+  int uvLevel = averageAnalogRead(UVOUT);
+  int refLevel = averageAnalogRead(REF_3V3);
+  
+  //Use the 3.3V power pin as a reference to get a very accurate output value from sensor
+  float outputVoltage = 3.3 / refLevel * uvLevel;
+  
+  float uvIntensity = mapfloat(outputVoltage, 0.99, 2.8, 0.0, 15.0); //Convert the voltage to a UV intensity level
+
+  return max(0.00,uvIntensity);
+}
+
+//Takes an average of readings on a given pin
+//Returns the average ---from ML8511 example
+int averageAnalogRead(int pinToRead)
+{
+  byte numberOfReadings = 8;
+  unsigned int runningValue = 0; 
+
+  for(int x = 0 ; x < numberOfReadings ; x++)
+    runningValue += analogRead(pinToRead);
+  runningValue /= numberOfReadings;
+
+  return(runningValue);  
+}
+
+//The Arduino Map function but for floats ---from ML8511 example
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+float get_current(){
+  float currentRead = float(analogRead(A6));
+  current = (currentRead/1024.f)*5.f/32.9f;
+  return (current);
+}
+
+float get_voltage(){
+  float voltageRead = float(analogRead(A3));
+  voltage = (voltageRead/1024.f)*5.f/0.3076f;
+  return (voltage);
+}
